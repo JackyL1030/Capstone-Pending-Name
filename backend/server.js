c1b08277a72291ab80fde9dceca0dfc89ab72296
Jacky <jackylai1030@gmail.com>
2026-08-03 13:50:27 -0400

    add initial environment configuration

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
-import express from 'express';
-import 'dotenv/config';
+import express from "express";
+import "dotenv/config";
 import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req,res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-})
+  console.log(`Server running at http://localhost:${PORT}`);
+});
