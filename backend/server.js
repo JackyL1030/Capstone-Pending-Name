b6e70247276d085caf1d7c6927a5fb4b6dab67fa
Jacky <jackylai1030@gmail.com>
2026-08-04 11:11:44 -0400

    import authRoutes to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import { connectDB } from './config/db.js'
+
+import { connectDB } from "./config/db.js"
+import authRoutes from "./routes/authRoutes.js"
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
