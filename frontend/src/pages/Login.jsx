96400e13fb662b090c521d81fc20a6a085c4473d
Jacky <jackylai1030@gmail.com>
2026-08-06 12:02:58 -0400

    created first controlled React form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // browser does not refresh
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(email);
+    console.log(password);
+  };
+
   return (
     <div>
       <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Log In</button>
+      </form>
     </div>
   );
 }
-
