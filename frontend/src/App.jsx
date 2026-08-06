1c2d0049a33d8b99ec6c83db7c42da33db52b7bf
Jacky <jackylai1030@gmail.com>
2026-08-06 15:22:53 -0400

    added role-based rotue protection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import ManagerDashboard from "./pages/ManagerDashboard";
 import EmployeeDashboard from "./pages/EmployeeDashboard";
-import ProtectedRoutes from "./components/ProtectedRoute";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route element={<ProtectedRoutes />}>
+        <Route element={<ProtectedRoute role="manager" />}>
           <Route path="/manager" element={<ManagerDashboard />} />
+        </Route>
+        <Route element={<ProtectedRoute role="employee" />}>
           <Route path="/employee" element={<EmployeeDashboard />} />
         </Route>
       </Routes>
--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,13 +2,17 @@ import { Navigate, Outlet } from "react-router-dom";
 import useAuth from "../context/useAuth";
 
 // protects routes by checking if a user is authenticated
-export default function ProtectedRoute() {
+export default function ProtectedRoute({ role }) {
   // gets current authentication state from AuthContext
   const { user } = useAuth();
   // if no user is loggined in, redirect them to the login page
   if (!user) {
     return <Navigate to="/login" />;
   }
-  // if user exists, allow access to the protected page
+  // checks if the user has the required role for this route
+  if (role && user.role !== role) {
+    return <Navigate to="/login" />;
+  }
+  // if authentication and role chcecks pass, allow access to the protected page
   return <Outlet />;
 }
