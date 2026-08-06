3766649d0292a43774a9f3ea883d582d42c7ceb7
Jacky <jackylai1030@gmail.com>
2026-08-06 11:24:41 -0400

    connect AuthProvider to application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRoute } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import AuthProvider from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRoute>
-      <App />
-    </BrowserRoute>
+    <AuthProvider>
+      <BrowserRoute>
+        <App />
+      </BrowserRoute>
+    </AuthProvider>
   </StrictMode>,
 );
