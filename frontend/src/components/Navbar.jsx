b117001efa098d442dd1599a653c5c053329b86a
Jacky <jackylai1030@gmail.com>
2026-08-06 15:37:15 -0400

    started on Navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import useAuth from "../context/useAuth";
+
+export default function Navbar() {
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  // logs out the user and redirects them to the login page
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+  return (
+    <nav>
+      <h1>Flexora</h1>
+      <div>
+        <span>
+          {user.name} ({user.role})
+        </span>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    </nav>
+  );
+}
