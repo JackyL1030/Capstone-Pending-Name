0f37f88a46f72981fa9e1114adf040d6f2484259
Jacky <jackylai1030@gmail.com>
2026-08-06 15:06:43 -0400

    implement protected routes to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import ManagerDashboard from "./pages/ManagerDashboard";
 import EmployeeDashboard from "./pages/EmployeeDashboard";
+import ProtectedRoutes from "./components/ProtectedRoute";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/manager" element={<ManagerDashboard />} />
-        <Route path="/employee" element={<EmployeeDashboard />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/manager" element={<ManagerDashboard />} />
+          <Route path="/employee" element={<EmployeeDashboard />} />
+        </Route>
       </Routes>
     </>
   );
