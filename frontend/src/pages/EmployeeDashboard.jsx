303121bf4a748c8d843117446a30b9f0a09b17b6
Jacky <jackylai1030@gmail.com>
2026-08-06 15:39:46 -0400

    added navbar to employee and manager dashboard

--- frontend/src/pages/EmployeeDashboard.jsx
+++ frontend/src/pages/EmployeeDashboard.jsx
@@ -1,5 +1,16 @@
-export default function EmployeeDashboard(){
-    return(
-        <h1>Employee Dashboard</h1>
-    )
+import useAuth from "../context/useAuth";
+import Navbar from "../components/Navbar";
+
+export default function EmployeeDashboard() {
+  const { user, token } = useAuth();
+
+  return (
+    <div>
+      <Navbar />
+      <h1>Employee Dashboard</h1>
+
+      <p>User: {user?.name}</p>
+      <p>Role: {user?.role}</p>
+    </div>
+  );
 }
--- frontend/src/pages/ManagerDashboard.jsx
+++ frontend/src/pages/ManagerDashboard.jsx
@@ -1,10 +1,12 @@
 import useAuth from "../context/useAuth";
+import Navbar from "../components/Navbar";
 
 export default function ManagerDashboard() {
   const { user, token } = useAuth();
 
   return (
     <div>
+      <Navbar />
       <h1>Manager Dashboard</h1>
 
       <p>User: {user?.name}</p>
