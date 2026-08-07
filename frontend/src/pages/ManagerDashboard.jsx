c24180a37d938481d62d996183b84b70e1b10f58
Jacky <jackylai1030@gmail.com>
2026-08-07 09:32:11 -0400

    imported components to manager dashboard

--- frontend/src/pages/ManagerDashboard.jsx
+++ frontend/src/pages/ManagerDashboard.jsx
@@ -1,5 +1,9 @@
 import useAuth from "../context/useAuth";
+
 import Navbar from "../components/Navbar";
+import ShiftForm from "../components/ShiftForm"
+import ShiftCalendar from "../components/ShiftCalendar"
+import ShiftTable from "../components/ShiftTable";
 
 export default function ManagerDashboard() {
   const { user } = useAuth();
@@ -9,8 +13,16 @@ export default function ManagerDashboard() {
       <Navbar />
       <div className="dashboard-container">
         <h1 className="dashboard-title">Manager Dashboard</h1>
-        <div className="dashboard-card">
-          <p>Manager tools will appear here.</p>
+        <div className="dashboard-grid">
+          <div className="dashboard-card">
+            <ShiftForm />
+          </div>
+          <div className="dashboard-card">
+            <ShiftCalendar />
+          </div>
+          <div className="dashboard-card">
+            <ShiftTable />
+          </div>
         </div>
       </div>
     </>
