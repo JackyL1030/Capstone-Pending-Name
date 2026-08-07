fe1e9c3ff4d5953a95f2b975859f554b52065a55
Jacky <jackylai1030@gmail.com>
2026-08-07 13:55:26 -0400

    fixed swap request drop down menu to exclude user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,20 +1,40 @@
 import User from "../models/User.js";
-// gets the current authenticated user
+
+// Get currently logged in user
 export const getMe = async (req, res) => {
-  res.status(200).json(req.user);
+  try {
+    const user = await User.findById(req.user.id)
+      .select("-password")
+      .populate("department", "name");
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+      error: error.message,
+    });
+  }
 };
 
+// Get all employees except the logged in employee
 export const getEmployees = async (req, res) => {
   try {
     const employees = await User.find({
       role: "employee",
-    }).select("name email");
+      _id: { $ne: req.user.id },
+    }).select("name email department");
 
     res.status(200).json(employees);
-
   } catch (error) {
     res.status(500).json({
-      message: error.message,
+      message: "Server Error",
+      error: error.message,
     });
   }
 };
